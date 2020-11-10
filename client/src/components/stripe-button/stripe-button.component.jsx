b790bb905f252d2841fa8968d0cde514d61b7b0e
EspressoTrip-v2 <juanbo.jb@gmail.com>
2020-11-10 02:16:18 +0200

    Separated app into client folder, added express server to process payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
+/* STRIPE CHECKOUT BUTTON */
 import StripeCheckout from 'react-stripe-checkout';
 
+/* AXIOS */
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price})=>{
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HjBxxIbGxMIzQER4SlEsQfkCYBaOs8us5mSC9PSbwAgk9gQVZqoahUlPfQr5hd0iHwRWFZhEbZ9O3ai4hBFBWWu00la2I1VaU'
     const onToken = token => {
-        alert('Payment Successful');
+        axios({
+            method: 'post',
+            url: '/payment',
+            data:{
+                token,
+                amount:priceForStripe   
+            }
+            
+        }).then(res =>alert(`Payment of $${price} successful`)).catch(err =>{
+            console.log('Payment Error:',err.message)
+            alert('Your payment has failed, please use the provided test credit card number.')
+        })
     }
     return (
         
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+/* ONLY LOAD ENV FILE IN DEVELOPEMENT */
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+/* PASS SECRET KEY TO STRIPE */
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+/* EXPRESS APP */
+const app = express();
+const port = process.env.PORT || 5000;
+
+/* MIDDLEWARE INCLUSION */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+/* SETUP FILE LOCATION FOR GET REQUESTS */
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+/* INSTANTIATE EXPRESS APP */
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log(`Server running on port ${port}`);
+});
+
+/* ROUTES */
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd',
+  };
+
+  stripe.charges.create(body, (stripeError, stripeResponse) => {
+    if (stripeError) {
+      console.log(stripeError.message);
+      res.status(500).send({ error: stripeError });
+    } else {
+      res.status(200).send({ success: stripeResponse });
+    }
+  });
+});
