0fba3cf4435684584a0b2ade0461e98060b3fa45
EspressoTrip-v2 <juanbo.jb@gmail.com>
2020-11-18 01:17:29 +0200

    Added compression in server file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 /* ONLY LOAD ENV FILE IN DEVELOPEMENT */
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -13,6 +14,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 /* MIDDLEWARE INCLUSION */
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
